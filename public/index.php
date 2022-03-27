9eabebcd28cd2c67ea4fb4ab370ea6354e9b7145
VinTesta <70596205+VinTesta@users.noreply.github.com>
2022-03-27 18:52:09 -0300

    /teste auth

--- public/index.php
+++ public/index.php
@@ -43,7 +43,7 @@ $callableResolver = $app->getCallableResolver();
 $tokenService = new TokenService();
 
 $app->add(new Tuupola\Middleware\JwtAuthentication([
-    "path" => [],
+    "path" => ["/teste"],
     "secret" => $tokenService->getKey(),
     "algorithm" => ["HS256"],
     "error" => function ($response, $arguments) {
