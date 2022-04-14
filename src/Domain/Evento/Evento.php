8590ee5a910bf23aecf4bd8f1edbfd0079621562
VinTesta <70596205+VinTesta@users.noreply.github.com>
2022-04-14 19:19:57 -0300

    Correção variaveis evento

--- src/Domain/Evento/Evento.php
+++ src/Domain/Evento/Evento.php
@@ -4,17 +4,17 @@ namespace App\Domain\Evento;
 class Evento
 {
     #region Parâmetros Privados
-    public $_idevento;
-    public $_tituloevento;
-    public $_descevento;
-    public $_dataevento;
+    public $idevento;
+    public $tituloevento;
+    public $descevento;
+    public $dataevento;
     #endregion
 
     public function __construct($params)
     {
-        $this->_idevento = isset($params["idevento"]) ? $params["idevento"] : null;
-        $this->_tituloevento = isset($params["tituloevento"]) ? $params["tituloevento"] : null;
-        $this->_descevento = isset($params["descevento"]) ? $params["descevento"] : null;
-        $this->_dataevento = isset($params["dataevento"]) ? $params["dataevento"] : null;
+        $this->idevento = isset($params["idevento"]) ? $params["idevento"] : null;
+        $this->tituloevento = isset($params["tituloevento"]) ? $params["tituloevento"] : null;
+        $this->descevento = isset($params["descevento"]) ? $params["descevento"] : null;
+        $this->dataevento = isset($params["dataevento"]) ? $params["dataevento"] : null;
     }
 }
